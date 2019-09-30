f741c83cd99a0bb90a3dc67c99594ef7b224575d
GreyGreyman <greygreyman@gmail.com>
2019-10-01 05:22:03 +1000

    Update feed parser
    Check required (according to apple rss spec) episode tags

--- src/feed_parsers.js
+++ src/feed_parsers.js
@@ -3,12 +3,18 @@ export function myFeedParser(xml) {
   const doc = domParser.parseFromString(xml, 'text/xml');
   const items = doc.querySelectorAll('item');
 
-  let episodes = Array.from(items).map((episode) =>
-    ({
-      title: episode.querySelector('title').textContent,
-      link: episode.querySelector('enclosure').getAttribute('url')
-    })
-  );
+  let episodes = Array.from(items).map((episode) => {
+
+    let title = episode.querySelector('title').textContent || 'error: missing title tag';
+    let enclosure = episode.querySelector('enclosure') || null;
+    let link = enclosure ? enclosure.getAttribute('url') : 'error: missing enclosure tag';
+
+    return ({
+      title,
+      link,
+    });
+  });
+
   episodes = episodes.slice(0, 15);
 
 
@@ -19,7 +25,7 @@ export function myFeedParser(xml) {
   description = description.reduce((max, string) => string.length > max.length ? string : max);
 
   let image = doc.querySelector('image[href]').getAttribute('href');
-  
+
   const podcast = {
     title: doc.querySelector('title').textContent,
     artist: doc.querySelector('author').textContent,
